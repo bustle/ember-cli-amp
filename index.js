12d4516b2f845f1e5b5c2a1a1933282d497982f4
Cory <cory.forsyth@gmail.com>
2016-09-01 15:15:44 -0400

    Add amp addon, build index, validate css

--- index.js
+++ index.js
@@ -1,136 +1,136 @@
 /*jshint node:true*/
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
-const CSSValidator = require('./lib/css_validator');
-const RSVP = require('rsvp');
-const Promise = RSVP.Promise;
+var fs = require('fs');
+var path = require('path');
+var CSSInjector = require('./lib/css_injector');
+var CSSValidator = require('./lib/css_validator');
 
-const TEMPLATE_PATH = path.resolve(__dirname, './ext/amp-index-template.html');
-const CSS_PLACEHOLDER = '<!-- EMBER_CLI_AMP_CSS_PLACEHOLDER -->';
-const CSS_PREFIX = '<style amp-custom>';
-const CSS_SUFFIX = '</style>';
+var TEMPLATE_PATH = path.join(__dirname, './ext/amp-index-template.html');
+var PLACEHOLDER = '<!-- EMBER_CLI_AMP_CSS_PLACEHOLDER -->';
 
 module.exports = {
   name: 'ember-cli-amp',
-  description: 'Render AMP with ember-cli and fastboot',
-
-  isDevelopingAddon() {
-    return true;
-  },
 
   /**
    * addon hook
+   * Runs when the addon is initially included by ember-cli
    * @see https://ember-cli.com/api/classes/Addon.html#method_included
    */
   included(app) {
-    this.options = this._initializeOptions(app.options);
+    var options       = app.options.amp || {};
+    this.cssPath      = options.css;
+    this.ampIndexPath = options.index;
+    this.buildCount   = 0;
+    this.alwaysRebuildIndex = options.alwaysRebuildIndex === undefined ? false : options.alwaysRebuildIndex;
+
+    if (!this.cssPath) {
+      throw new Error('You must specify amp.css option in ember-cli-build.js');
+    }
+    if (!this.ampIndexPath) {
+      throw new Error('You must specify amp.index option in ember-cli-build.js');
+    }
   },
 
   /**
    * addon hook
-   * Runs after ember-cli builds the app, and before syncing the built output
-   * to the outputPath
+   * Runs after ember-cli builds the app, but before syncing the built output
+   * to the outputPath. Changes made to the results directory will be synced to
+   * the built output.
    * @see https://ember-cli.com/api/classes/Addon.html#method_postBuild
    */
   postBuild(results) {
-    this.options.resultsDir = results.directory;
-
-    var css = this._readBuiltFile(this.options.paths.css);
-
-    return this._validateCSS(css).then(() => {
-      var html       = this._getHTML(css);
-      var outputPath = this._pathFor(this.options.paths.index);
-      fs.writeFileSync(outputPath, html);
-    });
+    if (this._shouldRebuildIndex()) {
+      return this._rebuildIndex(results).catch(err => {
+        this.ui.writeError('[amp-addon] Error building amp index: ' + err);
+      });
+    }
   },
 
   /**
-   * @return {Object}
+   * inject the CSS into the amp index template and write the index to the
+   * results directory
    */
-  _initializeOptions(appOptions) {
-    var options = {};
-    var amp = appOptions.amp || {};
-
-    options.paths = {
-      css:   amp.css,
-      index: amp.index
-    };
-
-    options.skipCSSValidation = !!amp.skipCSSValidation;
-
-    if (!options.paths.css) {
-      throw new Error('ember-cli-amp requires the amp.css option in ember-cli-build.js');
-    }
-    if (!options.paths.index) {
-      throw new Error('ember-cli-amp requires the amp.index option in ember-cli-build.js');
-    }
-
-    return options;
-  },
-
-  _getHTML(options) {
-    var css       = options.css;
+  _injectCSS(css) {
     var template  = this._readFile(TEMPLATE_PATH);
+    var outputPath = this._pathFor(this.ampIndexPath);
 
-    css = [CSS_PREFIX, css, CSS_SUFFIX].join('\n');
-    return template.replace(CSS_PLACEHOLDER, css);
+    var cssInjector = new CSSInjector({
+      css:         css,
+      template:    template,
+      placeholder: PLACEHOLDER
+    });
+
+    cssInjector.write(outputPath);
   },
 
-  // read a file at an absolute path
   _readFile(absolutePath) {
     return fs.readFileSync(absolutePath, 'utf8');
   },
 
-  // Read a file from a path relative to the results directory
   _readBuiltFile(relativePath) {
     return this._readFile(this._pathFor(relativePath));
   },
 
-  // return the absolute path of a relative path in the results directory
   _pathFor(relativePath) {
-    return path.join(this.options.resultsDir, relativePath);
+    return path.join(this.resultsDir, relativePath);
+  },
+
+  _shouldRebuildIndex() {
+    if (this.alwaysRebuildIndex) {
+      return true;
+    } else {
+      return this.buildCount === 0;
+    }
+  },
+
+  _rebuildIndex(results) {
+    this.buildCount++;
+
+    this.resultsDir = results.directory;
+    var css = this._readBuiltFile(this.cssPath);
+
+    return this._validateCSS(css)
+      .then(() => this._injectCSS(css));
   },
 
   _validateCSS(css) {
-    if (this.options.skipCSSValidation) {
-      return Promise.resolve();
-    }
-
     var validator = new CSSValidator();
-    return validator.validate(css).then(results => {
-      var ui = this.ui;
+    return validator.validate(css)
+      .then(results => this._logCSSResults(results));
+  },
 
-      if (results.errors.length) {
-        ui.writeError('AMP CSS failed validation: ');
+  _logCSSResults(results) {
+    var ui = this.ui;
 
-        results.errors.forEach(function(error) {
-          var code = error.code,
-              severity = error.severity,
-              detail = error.detail,
-              extraDetail = error.extraDetail,
-              specUrl = error.specUrl;
+    if (results.errors.length) {
+      ui.writeError('AMP CSS failed validation: ');
 
-          var msg = [];
-          if (detail) {
-            msg.push('Details: ' + detail);
-            if (extraDetail) {
-              msg.push('Other Details: ' + extraDetail);
-            }
-          } else if (extraDetail) {
-              msg.push('Details: ' + extraDetail);
+      results.errors.forEach(error => {
+        var code = error.code,
+            severity = error.severity,
+            detail = error.detail,
+            extraDetail = error.extraDetail,
+            specUrl = error.specUrl;
+
+        var msg = [];
+        if (detail) {
+          msg.push('Details: ' + detail);
+          if (extraDetail) {
+            msg.push('Other Details: ' + extraDetail);
           }
+        } else if (extraDetail) {
+            msg.push('Details: ' + extraDetail);
+        }
 
-          msg.push('Spec URL: ' + specUrl);
-          msg.push('Code: ' + code);
-          msg.push('Severity: ' + severity);
+        msg.push('Spec URL: ' + specUrl);
+        msg.push('Code: ' + code);
+        msg.push('Severity: ' + severity);
 
-          ui.writeError('  ' + msg.join('. '));
-        });
-      } else {
-        ui.writeInfoLine('AMP CSS validated');
-      }
-    });
+        ui.writeError('  ' + msg.join('. '));
+      });
+    } else {
+      ui.writeInfoLine('AMP CSS validated');
+    }
   }
 };
